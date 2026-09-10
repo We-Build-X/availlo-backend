d592afcfeeb699cbc105790e1fd17e3a77ecf839
abasiofon-sendan <abasiofon135@gmail.com>
2026-09-10 15:29:15 +0100

    updated to database connection route

--- classroom_radar/settings.py
+++ classroom_radar/settings.py
@@ -103,17 +103,17 @@ DATABASE_URL = os.environ.get("DATABASE_URL")
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 # if DATABASE_URL:
-#     DATABASES = {
-#         'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
-#     }
+DATABASES = {
+    'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+}
 
 # else:
-DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+#     DATABASES = {
+#             'default': {
+#                 'ENGINE': 'django.db.backends.sqlite3',
+#                 'NAME': BASE_DIR / 'db.sqlite3',
+#             }
+#         }
 
 
 # Password validation
